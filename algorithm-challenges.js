eba1d03759d9cb456781a59b01ac7dbb1ef443bb
Joshag <tech.gems.biz@gmail.com>
2024-05-31 14:33:49 +0100

    File rename

--- algorithm-challenges.js
+++ algorithm-challenges.js
@@ -1,20 +1,22 @@
 //@Search for longest word in a sentence
 const LongestWord = (sen) => {
   let longestWord;
-  //We use match regExp pattern to strip out any punctuation and
-  //match regExp method to return array
+  //We use match method of regExp that return a list
+  //form a pattern that strip-out whitespaces and punctuations
   sen = sen.match(/[A-Za-z0-9]+/gi);
 
-  //compare length of each array item and
-  //sort from the highest
+  //compare length of each word
+  //sorted in descending order
   const sorted = sen.sort((a, b) => b.length - a.length);
 
+  //take the first/longest word from the sorted word-list
   longestWord = sorted[0];
-  //return the first item since sorted from highest
+
+  //return the longest word
   return longestWord;
 };
 
-//@func Output:
+//@Run func. and print output
 console.info(
   LongestWord(
     "which is the best university in Nigeria to read computer science as of 2024?"
